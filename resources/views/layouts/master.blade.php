93f17f46f06fbf0cb20f4ac723b35ed360bb8660
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-07-04 20:15:17 -0400

    Change delete recording, yeah, !!! practice sitepoint

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">Tasks</a>
+            <a class="navbar-brand" href="{{route('home')}}">Tasks</a>
         </div>
         <div class="nav navbar-nav navbar-right">
             <li><a href="{{ route ('home') }}">Home</a></li>
